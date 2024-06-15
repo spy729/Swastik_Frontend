e6b1ddbf75574d30783342e5a193caab7d25131e
spy729 <spy729@users.noreply.github.com>
2024-06-15 16:20:59 +0530

    npm i styled-components

--- src/Component/HackathonCard.jsx
+++ src/Component/HackathonCard.jsx
@@ -5,42 +5,44 @@ import arrow from "../assets/arrow.png"
 import people from "../assets/people.png"
 import clock from "../assets/clock.png"
 import TiltComponent from "react-parallax-tilt";
+import styled from "styled-components";
+
 const HackathonCard = ({card}) => {
   const [isLiked,setLiked]=useState(true)
   return (
-    // <TiltComponent tiltMaxAngleX={5} tiltMaxAngleY={5} tiltReverse={true}>
-    // <div  className='bg-white relative w-[390px] h-[405px] rounded-xl border border-black'>
-    //   <img className='w-[390px] h-[120px] rounded-t-xl' src={card.bgimg}></img>
-    //   <img className='absolute bg-customGray h-[100px] w-[100px] object-contain rounded-full left-8 top-[5rem]' src={card.mainimg}></img>
-    //   <div className=' pl-5 pr-5 mt-[5rem] flex text-customGrayText justify-between'>
-    //     <div className='flex flex-col gap-9'>
-    //     <div className='flex flex-col gap-3 '>
-    //       <p className='flex gap-3'><span>{card.mode}</span>|<span>{card.cost}</span> </p>
-    //       <p className='text-black font-semibold scale-150 ml-7'>{card.name}</p>
-    //       <p>{card.location}</p>
-    //     </div>
-    //     <div className='flex flex-col gap-3'>
-    //     <p className='flex justify-start items-center gap-3'><img src={people}></img><p className='scale-90'>{card.participants}</p></p>
-    //     <p className='flex justify-start items-center gap-3'><img src={clock}></img><p className='scale-90'>{card.date}</p></p>
-    //     </div>
-    //     </div>
+    <TiltComponent tiltMaxAngleX={5} tiltMaxAngleY={5} tiltReverse={true}>
+    <div  className='bg-white relative w-[390px] h-[405px] rounded-xl border border-black'>
+      <img className='w-[390px] h-[120px] rounded-t-xl' src={card.bgimg}></img>
+      <img className='absolute bg-customGray h-[100px] w-[100px] object-contain rounded-full left-8 top-[5rem]' src={card.mainimg}></img>
+      <div className=' pl-5 pr-5 mt-[5rem] flex text-customGrayText justify-between'>
+        <div className='flex flex-col gap-9'>
+        <div className='flex flex-col gap-3 '>
+          <p className='flex gap-3'><span>{card.mode}</span>|<span>{card.cost}</span> </p>
+          <p className='text-black font-semibold scale-150 ml-7'>{card.name}</p>
+          <p>{card.location}</p>
+        </div>
+        <div className='flex flex-col gap-3'>
+        <p className='flex justify-start items-center gap-3'><img src={people}></img><p className='scale-90'>{card.participants}</p></p>
+        <p className='flex justify-start items-center gap-3'><img src={clock}></img><p className='scale-90'>{card.date}</p></p>
+        </div>
+        </div>
 
       
-    //   <div className='flex flex-col justify-between'>
-    //   {
-    //     isLiked ?
-    //     <img onClick={()=>setLiked(!isLiked)} className='h-[30px] w-[30px] object-contain' src={hart1}></img>
-    //     :
-    //     <img onClick={()=>setLiked(!isLiked)} className='h-[30px] w-[30px] object-contain' src={hart2}></img>
-    //   }
+      <div className='flex flex-col justify-between'>
+      {
+        isLiked ?
+        <img onClick={()=>setLiked(!isLiked)} className='h-[30px] w-[30px] object-contain' src={hart1}></img>
+        :
+        <img onClick={()=>setLiked(!isLiked)} className='h-[30px] w-[30px] object-contain' src={hart2}></img>
+      }
       
-    //   <img className='h-[20px] w-[20px] object-contain mb-3' src={arrow}></img>
-    //   </div>
-    //   </div>
+      <img className='h-[20px] w-[20px] object-contain mb-3' src={arrow}></img>
+      </div>
+      </div>
      
-    // </div>
-    // </TiltComponent>
-    <p>Tilt</p>
+    </div>
+    </TiltComponent>
+    // <p>Tilt</p>
   )
 }
 
